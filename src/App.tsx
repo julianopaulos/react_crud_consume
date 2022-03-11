ddbd917b36c6b802352b3b47df7b594a7c5348d4
julianopaulos <julianopaulo.santos@hotmail.com>
2022-03-11 06:11:08 -0300

    melhorando início do roteamento e começando a estudar context para usar no projeto

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,12 @@
-import React from "react";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Routes from './routes';
 
 function App() {
   return (
-    <BrowserRouter>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">about</Link>
-            </li>
-            <li>
-              <Link to="/users">Users</Link>
-            </li>
-          </ul>
-        </nav>
-        <Routes>
-          <Route path="/about" element={<About />} />
-          <Route path="/users" element={<Users />} />
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </div>
-    </BrowserRouter>
+        <>
+            <Routes/>
+        </>
   );
 }
-function Home() {
-  return <h2>Home</h2>;
-}
 
-function About() {
-  return <h2>About</h2>;
-}
-
-function Users() {
-  return <h2>Users</h2>;
-}
 
 export default App;
